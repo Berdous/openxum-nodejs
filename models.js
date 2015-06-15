b106e1f4fe1adf69d7309135c663810638ff5779
Slimane <berdouss2@gmail.com>
2015-06-15 16:25:47 +0000

    ajouter les controles de champs et le style pour la page du chat

--- models.js
+++ models.js
@@ -16,5 +16,6 @@ exports = module.exports = function (app, mongoose) {
 
     require('./schema/GameType')(app, mongoose);
     require('./schema/Game')(app, mongoose);
-	 require('./schema/GameHisto')(app, mongoose);
+	require('./schema/GameHisto')(app, mongoose);
+    //require('./schema/Connected')(app, mongoose);
 };
